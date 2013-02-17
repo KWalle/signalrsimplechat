1f99a322c0ebc2b64ac0abfe6de5b2cb1343e447
kristian.walle <kristian.walle@zenbook>
2013-02-17 15:10:43 +0100

    Added additional methods to send messages (self, others)

--- SignalRSimpleChat/ChatHub.cs
+++ SignalRSimpleChat/ChatHub.cs
@@ -4,9 +4,19 @@ namespace SignalRSimpleChat
 {
     public class ChatHub : Hub
     {
-         public void Send(string message)
-         {
-             Clients.All.sendMessage(message);
-         }
+        public void Send(string message)
+        {
+            Clients.All.sendMessage(message);
+        }
+
+        public void OnlyToMe(string message)
+        {
+            Clients.Caller.sendMessage(message);
+        }
+
+        public void ToEveryoneElse(string message)
+        {
+            Clients.Others.sendMessage(message);
+        }
     }
 }
