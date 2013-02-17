6e939041d722a5fd7fc495a124f83158480f342f
kristian.walle <kristian.walle@zenbook>
2013-02-17 14:53:14 +0100

    Added ChatHub and initial send method

---
+++ SignalRSimpleChat/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRSimpleChat
+{
+    public class ChatHub : Hub
+    {
+         public void Send(string message)
+         {
+             Clients.All.sendMessage(message);
+         }
+    }
+}
