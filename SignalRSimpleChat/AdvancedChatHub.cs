07d1a76e155babde1c483b1f359a691d098c7774
kristian.walle <kristian.walle@gmail.com>
2013-02-17 18:04:29 +0100

    Added AdvancedChat with c# type

---
+++ SignalRSimpleChat/AdvancedChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRSimpleChat
+{
+    public class AdvancedChatHub : Hub
+    {
+         public void SendMessage(ChatMessage message)
+         {
+             Clients.All.sendChatMessage(message);
+         }
+    }
+}
---
+++ SignalRSimpleChat/ChatMessage.cs
@@ -0,0 +1,8 @@
+namespace SignalRSimpleChat
+{
+    public class ChatMessage
+    {
+        public string UserName { get; set; }
+        public string Message { get; set; }
+    }
+}
