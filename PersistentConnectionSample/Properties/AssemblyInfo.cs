6cb745f2cebc9d7adad615225a6232b6d051311e
kristian.walle <kristian.walle@gmail.com>
2013-02-19 21:13:23 +0100

    Added PersistentConnection working sample

---
+++ PersistentConnectionSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PersistentConnectionSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PersistentConnectionSample")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c3c9e1a-7efa-49a6-837c-95aa200fc9de")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PersistentConnectionSample/TrackerConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
+
+namespace PersistentConnectionSample
+{
+    public class TrackerConnection : PersistentConnection
+    {
+        public static void InitRoute()
+        {
+            RouteTable.Routes.MapConnection<TrackerConnection>(name: "tracker", url: "/tracker");
+        }
+
+        protected override System.Threading.Tasks.Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            return Connection.Broadcast(data, connectionId);
+        }
+    }
+}
